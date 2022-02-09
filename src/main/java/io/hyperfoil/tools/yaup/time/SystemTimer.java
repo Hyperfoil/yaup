2eadda104e4fa5236eae0bd349d8911bbb534bd9
Will Reichert <willr3@gmail.com>
2022-02-08 20:23:45 -0500

    add data to SytemTimers

--- src/main/java/io/hyperfoil/tools/yaup/time/SystemTimer.java
+++ src/main/java/io/hyperfoil/tools/yaup/time/SystemTimer.java
@@ -22,7 +22,7 @@ public class SystemTimer {
     // A black hole to avoid returning null for SystemTimer.current()
     private static final SystemTimer NULL = new SystemTimer("NULL"){
         @Override
-        public SystemTimer start(String name, boolean parallel){return this;}
+        public SystemTimer start(String name, Json json, boolean parallel){return this;}
         @Override
         public boolean isActive(){return false;}
     };
@@ -34,7 +34,9 @@ public class SystemTimer {
     private long nanoStop=-1;
     private long milliStop=-1;
     private String name;
+    private Json data;
 
+    private SystemTimer parent;
     private List<SystemTimer> children;
 
 
@@ -48,6 +50,8 @@ public class SystemTimer {
         this.nanoStart = nano;
         this.isParallel = isParallel;
         this.hasParallel = false;
+        this.parent = null;
+        this.data = new Json(false);
     }
 
     /**
@@ -55,12 +59,23 @@ public class SystemTimer {
      * @return true if the timer was started, or false if already active
      */
     public boolean start(){
-//TODO throwing breaks async ScriptCmd
-//        if(isStopped()){
-//            throw new IllegalStateException("cannot start after stop("+name+")");
-//        }
-        if(isActive()){
+        return start((Json) null);
+    }
+
+    public boolean hasParent(){return parent !=null && parent != NULL;}
+    public SystemTimer getParent(){return parent;}
+
+    /**
+     * Starts the current timer if it was not already started
+     * @param data additional data to store with the timer
+     * @return
+     */
+    public boolean start(Json data){
+        if(isStopped() || isActive()){
             return false;
+        }
+        if(data!=null){
+            this.data.merge(data);
         }
         this.milliStart = System.currentTimeMillis();
         this.nanoStart = System.nanoTime();
@@ -78,7 +93,7 @@ public class SystemTimer {
 
 
     public SystemTimer start(String name){
-        return start(name,false);
+        return start(name,null,false);
     }
     /**
      * Starts a new child timer and ends the current child timer if !parallel.
@@ -87,6 +102,16 @@ public class SystemTimer {
      * @return new SystemTime
      */
     public SystemTimer start(String name, boolean parallel){
+        return start(name,null,parallel);
+    }
+    /**
+     * Starts a new child timer and ends the current child timer if !parallel.
+     * @param name the name of the new timer. Names are not unique
+     * @param data to associate with the new SystemTimer
+     * @param parallel if true, leave current child timer running, otherwise stop child timers
+     * @return new SystemTime
+     */
+    public SystemTimer start(String name, Json data, boolean parallel){
 //TODO throwing breaks async ScriptCmd
 //        if(isStopped()){
 //            throw new IllegalStateException("cannot start("+name.trim()+") after stop("+this.name+")");
@@ -97,9 +122,10 @@ public class SystemTimer {
         long nano = System.nanoTime();
         if(isActive()) {
             if(!parallel) {
-                stopChildren(milli,nano,false);
+                stopChildren(milli,nano,null,false);
             }
         }else{
+            //this isn't started so it needs to start too
             this.milliStart = milli;
             this.nanoStart = nano;
 
@@ -108,18 +134,26 @@ public class SystemTimer {
         milli = System.currentTimeMillis();
         nano = System.nanoTime();
         SystemTimer newTimer =new SystemTimer(name,milli,nano,parallel);
+        newTimer.parent = this;
+        newTimer.data.merge(data);
         children.add(newTimer);
         return newTimer;
     }
+
+    public Json getData(){return data;}
 
     /**
      * Stop this timer and all descendant timers
      */
     public void stop(){
+        stop((Json)null);
+    }
+
+    public void stop(Json data){
         if(isActive()) {
             long milli = System.currentTimeMillis();
             long nano = System.nanoTime();
-            this.stop(milli, nano, true);
+            this.stop(milli, nano, data,true);
         }
     }
     public boolean hasChildren(){return !children.isEmpty();}
@@ -135,10 +169,13 @@ public class SystemTimer {
         }
     }
     private void stop(long milliStop,long nanoStop,boolean stopParallel){
+        stop(milliStop,nanoStop,null,stopParallel);
+    }
+    private void stop(long milliStop,long nanoStop,Json data,boolean stopParallel){
         if(isActive() && ( stopParallel || !isParallel)) {
             this.milliStop = milliStop;
             this.nanoStop = nanoStop;
-            stopChildren(milliStop,nanoStop,stopParallel);
+            stopChildren(milliStop,nanoStop,data,stopParallel);
         }
     }
 
@@ -149,10 +186,10 @@ public class SystemTimer {
      * @param nanoStop
      * @param stopParallel
      */
-    private void stopChildren(long milliStop,long nanoStop,boolean stopParallel){
+    private void stopChildren(long milliStop,long nanoStop,Json data, boolean stopParallel){
         int counter = children.size()-1;
         while(counter>=0 && ( (stopParallel && hasParallel) || children.get(counter).isActive())){
-            children.get(counter).stop(milliStop, nanoStop, stopParallel);
+            children.get(counter).stop(milliStop, nanoStop, data,stopParallel);
             counter--;
         }
         if(stopParallel){
@@ -168,6 +205,10 @@ public class SystemTimer {
         rtrn.set("stop",getStop());
         rtrn.set("nanos",nanoTime());
         rtrn.set("millis",milliTime());
+        if(!data.isEmpty()) {
+            rtrn.set("data", data.clone());
+        }
+
         if(hasChildren()){
             Json childrenJson = new Json();
             for(int i=0; i<children.size(); i++){ //was getting ConcurrentModificationException, want snapshot read?
