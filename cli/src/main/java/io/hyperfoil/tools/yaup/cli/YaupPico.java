567076575345074ccc91fdf2f5fa3a89a42cadaf
Will Reichert <willr3@gmail.com>
2024-09-18 19:45:35 -0400

    add -j, --json to xml command

--- cli/src/main/java/io/hyperfoil/tools/yaup/cli/YaupPico.java
+++ cli/src/main/java/io/hyperfoil/tools/yaup/cli/YaupPico.java
@@ -30,12 +30,18 @@ public class YaupPico implements QuarkusApplication {
         return cmd.execute(args);
     }
 
-    @CommandLine.Command(name="xml", description="perform the operation on an xml document")
-    public int xml(String operation,String path){
+    @CommandLine.Command(name="xml", description="perform the operation on an xml document", mixinStandardHelpOptions = true)
+    public int xml(@CommandLine.Option(names={"-j","--json"}) boolean json, String operation, String path){
         Xml doc = Xml.parseFile(path);
         String response = doc.apply(operation);
         logger.infof("xml response:%n%s",response);
-        System.out.printf("%s%n",response);
+        if(json ){
+            Xml found = Xml.parse(response);
+            if( found.exists()){
+                response = found.toJson().toString();
+            }
+        }
+        System.out.printf("%s%n", response);
         return 0;
     }
     @CommandLine.Command(name="json-diff", description = "compare two json objects")
