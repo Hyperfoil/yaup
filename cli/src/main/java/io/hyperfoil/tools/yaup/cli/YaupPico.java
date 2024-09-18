ed038216adda9b08d058a3baad7ad407a1d9f648
Will Reichert <willr3@gmail.com>
2024-09-18 14:35:07 -0400

    use system.out.printf for console output from cli

--- cli/src/main/java/io/hyperfoil/tools/yaup/cli/YaupPico.java
+++ cli/src/main/java/io/hyperfoil/tools/yaup/cli/YaupPico.java
@@ -1,7 +1,9 @@
 package io.hyperfoil.tools.yaup.cli;
 
 import io.hyperfoil.tools.yaup.json.Json;
+import io.hyperfoil.tools.yaup.json.JsonComparison;
 import io.hyperfoil.tools.yaup.json.JsonValidator;
+import io.hyperfoil.tools.yaup.xml.XmlLoader;
 import io.hyperfoil.tools.yaup.xml.XmlOperation;
 import io.hyperfoil.tools.yaup.xml.pojo.Xml;
 import io.quarkus.picocli.runtime.annotations.TopCommand;
@@ -11,6 +13,7 @@ import picocli.AutoComplete;
 import picocli.CommandLine;
 
 import java.lang.invoke.MethodHandles;
+import java.nio.file.Path;
 import java.util.List;
 
 @TopCommand
@@ -29,20 +32,38 @@ public class YaupPico implements QuarkusApplication {
 
     @CommandLine.Command(name="xml", description="perform the operation on an xml document")
     public int xml(String operation,String path){
-        XmlOperation xmlOperation = XmlOperation.parse(operation);
         Xml doc = Xml.parseFile(path);
         String response = doc.apply(operation);
-        logger.infof("response:%n%s",response);
+        logger.infof("xml response:%n%s",response);
+        System.out.printf("%s%n",response);
+        return 0;
+    }
+    @CommandLine.Command(name="json-diff", description = "compare two json objects")
+    public int jsonDiff(String firstPath,String secondPath){
+        Json firstJson = Json.fromFile(firstPath);
+        Json secondJson = Json.fromFile(secondPath);
+
+        JsonComparison comparison = new JsonComparison();
+        comparison.load("A",firstJson);
+        comparison.load("B",secondJson);
+
+        int length = comparison.jsonNames().stream().mapToInt(String::length).max().orElseGet(()->1);
+        comparison.getDiffs().forEach(entry->{
+            System.out.printf("%s%n",entry.getPath());
+            entry.forEach((key,value)->{
+                System.out.printf("%"+length+"s : %s%n",key,value.length() < 40 ? value : value.substring(0,40)+"...");
+            });
+        });
         return 0;
     }
     @CommandLine.Command(name="json-schema", description="validate json with a schema",aliases = {"schema"})
     public int jsonSchema(String schemaPath,String jsonPath){
         Json schemaJson = Json.fromFile(schemaPath);
         Json dataJson = Json.fromFile(jsonPath);
-
         JsonValidator validator = new JsonValidator(schemaJson);
         Json errors = validator.validate(dataJson);
         logger.info(errors.toString());
+        System.out.printf("%s%n", errors.toString(2));
         return 0;
     }
     @CommandLine.Command(name="type-structure", description="create a type structure from the provided json files",aliases = {"shape","typestructure","structure"})
